7537c856862663b809fd1942fe57dd6746a8ebb9
zyuiop <zyuiop@zyuiop.net>
2016-10-10 00:47:57 +0200

    Allow to give money to an other city

--- src/main/java/net/zyuiop/rpmachine/cities/commands/citysubcommands/GiveMoneyCommand.java
+++ src/main/java/net/zyuiop/rpmachine/cities/commands/citysubcommands/GiveMoneyCommand.java
@@ -5,6 +5,7 @@ import net.zyuiop.rpmachine.cities.CitiesManager;
 import net.zyuiop.rpmachine.cities.commands.SubCommand;
 import net.zyuiop.rpmachine.cities.data.City;
 import net.zyuiop.rpmachine.database.FinancialCallback;
+import net.zyuiop.rpmachine.economy.TaxPayerToken;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -20,12 +21,12 @@ public class GiveMoneyCommand implements SubCommand {
 
 	@Override
 	public String getUsage() {
-		return "<montant à donner>";
+		return "<montant à donner> [nom de la ville]";
 	}
 
 	@Override
 	public String getDescription() {
-		return "Vous permet d'effectuer un don de monnaie à votre ville.";
+		return "Vous permet d'effectuer un don de monnaie à une ville (ou la votre par défaut).";
 	}
 
 	@Override
@@ -33,11 +34,18 @@ public class GiveMoneyCommand implements SubCommand {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			City city = citiesManager.getPlayerCity(player.getUniqueId());
-			if (city == null) {
+			if (city == null && args.length < 2) {
 				player.sendMessage(ChatColor.RED + "Vous n'êtes membre d'aucune ville.");
+				return;
+			} else if (city == null) {
+				city = citiesManager.getCity(args[1]);
+			}
+
+			if (city == null) {
+				player.sendMessage(ChatColor.RED + "Cette ville n'existe pas.");
 			} else {
 				if (args.length < 1) {
-					player.sendMessage(ChatColor.RED + "Utilisation incorrecte : /city givemoney <montant>");
+					player.sendMessage(ChatColor.RED + "Utilisation incorrecte : /city givemoney " + getUsage());
 				} else {
 					String amtStr = args[0];
 					try {
@@ -48,17 +56,16 @@ public class GiveMoneyCommand implements SubCommand {
 						if (!RPMachine.getInstance().getEconomyManager().canPay(player.getUniqueId(), amt)) {
 							player.sendMessage(ChatColor.RED + "Vous ne pouvez pas payer ce montant.");
 						} else {
-							RPMachine.getInstance().getEconomyManager().withdrawMoneyWithBalanceCheck(player.getUniqueId(), amt, new FinancialCallback() {
-								@Override
-								public void done(double newAmount, double difference) {
-									if (difference < 0) {
-										city.setMoney(city.getMoney() + (difference * -1));
-										citiesManager.saveCity(city);
-										player.sendMessage(ChatColor.GREEN + "L'argent a bien été transféré.");
-									} else {
-										player.sendMessage(ChatColor.RED + "La transaction a échoué.");
-									}
+							TaxPayerToken token = RPMachine.getPlayerRoleToken(player);
+
+							City finalCity = city;
+							RPMachine.getInstance().getEconomyManager().transferMoneyBalanceCheck(token.getTaxPayer(), city, amt, result -> {
+								if (result) {
+									player.sendMessage(ChatColor.GREEN + "L'argent a bien été transféré.");
+								} else {
+									player.sendMessage(ChatColor.RED + "La transaction a échoué.");
 								}
+								citiesManager.saveCity(finalCity);
 							});
 						}
 					} catch (Exception e) {
