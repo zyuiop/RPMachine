eff10305ed7f9300d861452f9dac2b55fb19991b
zyuiop <zyuiop@zyuiop.net>
2017-02-10 15:45:46 +0100

    ...

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,7 @@
 cp ../target/rpmachine-1.0-SNAPSHOT.jar ../rpmachine.jar
 
 # Then we deploy it using SSH because I still don't want to learn how to use rsync o/
+
 targetName=rpmachine-$TRAVIS_BUILD_NUMBER-${TRAVIS_COMMIT:0:8}.jar
 scp -oStrictHostKeyChecking=no rpmachine.jar deploy@ks.zyuiop.net:/srv/archive.zyuiop.net/RPMachine/$targetName
 ssh -oStrictHostKeyChecking=no deploy@ks.zyuiop.net cp /srv/archive.zyuiop.net/RPMachine/$targetName /srv/archive.zyuiop.net/RPMachine/rpmachine-LATEST.jar
