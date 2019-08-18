0d5b59d9172c7bd5b089120188cc0adaea78df8a
zyuiop <zyuiop@zyuiop.net>
2019-08-19 00:57:04 +0200

    Restrict collection

--- src/main/java/net/zyuiop/rpmachine/database/PlayerData.java
+++ src/main/java/net/zyuiop/rpmachine/database/PlayerData.java
@@ -1,43 +1,191 @@
 package net.zyuiop.rpmachine.database;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.Sets;
 import net.zyuiop.rpmachine.RPMachine;
 import net.zyuiop.rpmachine.common.VirtualLocation;
+import net.zyuiop.rpmachine.database.PlayerData;
 import net.zyuiop.rpmachine.entities.LegalEntity;
 import net.zyuiop.rpmachine.permissions.DelegatedPermission;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-
-import java.util.UUID;
 
 /**
  * @author zyuiop
  */
-public interface PlayerData extends LegalEntity {
-	String getJob();
+public class PlayerData implements LegalEntity {
+	private final YamlConfiguration data;
+	private final File file;
+	private final UUID uuid;
 
-	void setJob(String job);
+	public PlayerData(UUID uuid, YamlConfiguration data, File file) {
+		this.data = data;
+		this.file = file;
+		this.uuid = uuid;
+	}
 
-	VirtualLocation getHome();
+	public String getJob() {
+		return data.getString("job", null);
+	}
 
-	void setHome(VirtualLocation location);
+	public void setJob(String job) {
+		data.set("job", job);
+		save();
+	}
 
-	boolean isNew();
-
-	UUID getUuid();
-
-	default String getName() {
+	public String getName() {
 		return RPMachine.database().getUUIDTranslator().getName(getUuid());
 	}
 
-	boolean togglePlotMessages();
+	@Override
+	public boolean hasDelegatedPermission(Player player, DelegatedPermission permission) {
+		return player.getUniqueId().equals(getUuid()); // Player has all permissions on properties he manages!
+	}
 
-	void setAttribute(String key, Object value);
+	private void save() {
+		try {
+			data.save(file);
+		} catch (IOException e) {
+			RPMachine.getInstance().getLogger().severe("Error while saving playerData in " + file.getAbsolutePath());
+			e.printStackTrace();
+		}
+	}
 
-	<T> T getAttribute(String key);
+	public VirtualLocation getHome() {
+		String home = data.getString("rp.home", null);
+		if (home == null) {
+			return null;
+		}
 
-	boolean hasAttribute(String key);
+		return new VirtualLocation(home);
+	}
+
+	public void setHome(VirtualLocation home) {
+		data.set("rp.home", home.toString());
+		save();
+	}
 
 	@Override
-	default boolean hasDelegatedPermission(Player player, DelegatedPermission permission) {
-		return player.getUniqueId().equals(getUuid()); // Player has all permissions on properties he manages!
+	public boolean canActAs(Player p) {
+		return p.getUniqueId().equals(getUuid());
+	}
+
+	@Override
+	public double getBalance() {
+		return Math.round(data.getDouble("rpmoney", 0.0) * 100) / 100;
+	}
+
+	@Override
+	public void setBalance(double amount) {
+		data.set("rpmoney", amount);
+		save();
+	}
+
+	@Override
+	public boolean withdrawMoney(double amount) {
+		if (data.getDouble("rpmoney", 0D) >= amount) {
+			data.set("rpmoney", data.getDouble("rpmoney", 0D) - amount);
+			save();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void creditMoney(double amount) {
+		data.set("rpmoney", data.getDouble("rpmoney", 0D) + amount);
+		save();
+	}
+
+	@Override
+	public void setUnpaidTaxes(String city, double amount) {
+		if (amount <= 0) {
+			data.set("unpaid." + city, null);
+		} else {
+			data.set("unpaid." + city, amount);
+		}
+		save();
+	}
+
+	@Override
+	public double getUnpaidTaxes(String city) {
+		return data.getDouble("topay." + city, 0);
+	}
+
+	@Override
+	public void setLastTaxes(String city, Date nd) {
+		GregorianCalendar date = new GregorianCalendar();
+		date.setTime(nd);
+		String dateString = date.get(Calendar.DAY_OF_MONTH) + "/" + date.get(Calendar.MONTH) + "/" + date.get(Calendar.YEAR);
+
+		data.set("lasttaxes." + city, dateString);
+		save();
+	}
+
+	@Override
+	public Date getLastTaxes(String city) {
+		String data = this.data.getString("lasttaxes." + city, "none");
+		if (data.equals("none")) {
+			return null;
+		}
+
+		String[] parts = data.split("/");
+		GregorianCalendar calendar = new GregorianCalendar(Integer.parseInt(parts[2]), Integer.parseInt(parts[1]), Integer.parseInt(parts[0]));
+		return calendar.getTime();
+	}
+
+	@Override
+	public Map<String, Double> getUnpaidTaxes() {
+		Set<String> dataKeys = data.getKeys(false);
+		return dataKeys.stream()
+				.filter(key -> key.startsWith("topay."))
+				.collect(Collectors.toMap(key -> key.split("\\.")[1], data::getDouble));
+	}
+
+	@Override
+	public String displayable() {
+		return ChatColor.YELLOW + "(Joueur) " + ChatColor.GOLD + getName();
+	}
+
+	@Override
+	public String shortDisplayable() {
+		return ChatColor.GOLD + getName();
+	}
+
+	@Override
+	public Set<UUID> getAdministrators() {
+		return Sets.newHashSet(uuid);
+	}
+
+	public boolean isNew() {
+		return !data.getKeys(false).contains("rpmoney");
+	}
+
+	public boolean togglePlotMessages() {
+		boolean val = !data.getBoolean("seemessages", true);
+		data.set("seemessages", val);
+		return val;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setAttribute(String key, Object value) {
+		data.set("attr." + key, value);
+		save();
+	}
+
+	public boolean hasAttribute(String key) {
+		return data.contains("attr." + key);
+	}
+
+	public <T> T getAttribute(String key) {
+		return (T) data.get("attr." + key);
 	}
 }
